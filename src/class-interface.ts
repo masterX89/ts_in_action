0d4748df919913ad8859fa863638ed7e6228b92c
hk.xia <595424932@qq.com>
2022-06-14 07:13:04 +0800

    feat: class-interface

---
+++ src/class-interface.ts
@@ -0,0 +1,64 @@
+// class vs. interface
+// excalidraw: ./assets/class-interface
+// 现在看到的源码中 class 更偏向于应对 实例化 的场景
+// 而 interface 更倾向于 约束 对象的 props
+interface Human {
+  name: string
+  eat(): void
+}
+
+// 对象 接口 类型
+const jack: Human = {
+  name: 'Jack',
+  eat() {},
+}
+
+// class 和 interface 的关系
+class Asian implements Human {
+  constructor(name: string) {
+    this.name = name
+  }
+  // 接口只能约束 class 的共有成员
+  // private name: string
+  name: string
+  eat() {}
+}
+
+interface Man extends Human {
+  run(): void
+}
+interface Child {
+  cry(): void
+}
+// 合并了多个 interface
+interface Boy extends Man, Child {}
+
+// 对象类型接口
+const boy: Boy = {
+  name: 'John',
+  eat() {},
+  run() {},
+  cry() {},
+}
+
+// 接口继承类
+// 接口 抽象了 类的成员 而无具体实现
+class Auto {
+  state: number = 1
+}
+interface AutoInterface extends Auto {}
+// 对象接口类型
+const auto: AutoInterface = {
+  state: 2,
+}
+class C implements AutoInterface {
+  // 无需声明 state 因为从 interface 中实现了
+  state = 3
+}
+
+console.log(Auto)
+console.log(auto)
+console.log(C)
+
+class Bus extends Auto implements AutoInterface {}
+console.log(Bus)
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@
 // import './enum'
 // import './interface'
 // import './function'
-import './class'
+// import './class'
+import './class-interface'
 let hello: string = 'Hello TypeScript'
 document.querySelectorAll('.app')[0].innerHTML = hello
 // document.querySelector('.app').innerHTML = hello // Object is possible 'null'
